573c871f17abf6ec5ff0c35eca0ddc22c234e191
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-04-13 15:03:58 +0700

    JS refresh

---
+++ javaScript.js
@@ -0,0 +1,24 @@
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.push(7);
+// console.log(numbers);
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(number => {
+//     console.log(number);
+// });
+
+// let numbers = [1, 2, 3, 4, 5];
+// let index = 2;
+// console.log(numbers[index]);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let index = 2;
+// numbers.splice(index, 1);
+// console.log(numbers);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let filteredNumbers = numbers.filter(number => number % 2 === 0);
+// console.log(filteredNumbers);
+
+let numbers = [1, 2, 3, 4, 5];
+let transformedNumbers = numbers.map(number => number * 2);
+console.log(transformedNumbers);
